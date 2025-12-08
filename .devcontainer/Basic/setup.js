3f104e1329c5f23d176c4b217a283b0e9a2aecae
Vaibhav-8299 <1982vaibhavdubey@gmail.com>
2025-12-08 13:27:59 +0000

    Updated README with learning details

---
+++ .devcontainer/Basic/setup.js
@@ -0,0 +1,30 @@
+/*
+#How to create GitHub repository
+
+
+
+1.Go to github.com and log in.
+
+2.Click the “New” or “+ → New Repository” button.
+
+3.Give your repo a name.
+
+4.Choose Public or Private.
+
+5.Check the option “Add a README file”.
+
+6.Click Create Repository. 
+
+
+#Setup of Enviroment.
+
+
+
+1. create codespace 
+2. Create a dev container.
+3. install node js & js 
+4. select version 18 bookworm.
+5. click ok.
+6. create a folder inside dev container .
+7. create a file inside folder with .js extension.
+8. Run the file using command node filepath.*/
