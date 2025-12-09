f78dd6aff630b1f92b9c881e9168488be59dff27
Vaibhav-8299 <1982vaibhavdubey@gmail.com>
2025-12-09 05:29:34 +0000

    day2 studies

---
+++ .devcontainer/Basic/Day2.js
@@ -0,0 +1,40 @@
+/* a=23;
+const b ="abc";
+var c =78;
+d=88;
+console.log("values",a,b,c,d); //values are printed.
+
+
+a=3;
+//b=4; //TypeError: Assignment to constant variable.
+c=5
+
+//let a=5;
+//const b=6;
+//var c=9;
+
+console.log("values" ,a,b,c); */
+
+
+let a ="abc"
+let b="";
+let c=null; //null is object
+let d;     // its type is undefined
+console.log("values of a" ,a)
+console.log("values of b" ,b)
+console.log("values of c" ,c)
+console.log("values of d" ,d)
+
+
+console.log(typeof(a)); // return type of value in variable a.
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+
+
+let score = "33abcd";
+console.log(typeof(score));
+console.log(typeof score);
+let scoree = Number(score); //typecasting
+console.log(typeof scoree);
+console.log(scoree); //not a number
