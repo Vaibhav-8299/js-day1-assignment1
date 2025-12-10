675449efe9b6d660fb80b9c19729d0bcb0aae40e
Vaibhav-8299 <1982vaibhavdubey@gmail.com>
2025-12-10 05:31:11 +0000

    day3

---
+++ .devcontainer/Basic/day3.js
@@ -0,0 +1,52 @@
+/* function Run(n){
+    return n*2;
+}
+let ch= Run(23);
+console.log(ch);
+
+
+let arr=[1,2,3,4,"ab","abc","abcd",ch];
+console.log(arr);
+
+*/
+
+/* let fruit= ["apple","banana","kela","seb","santra"];
+for(let temp of fruit){
+console.log(temp);
+}
+fruit.push("papaya")
+console.log(fruit);
+fruit.unshit("kiwi")
+console.log(fruit);
+fruit.pop();
+console.log(fruit);
+fruit.includes("seb");
+console.log(fruit);
+fruit.indexOf("banana")
+console.log(fruit); */
+
+/*
+const numbers1 = [2, 4, 6, 8];
+const doubled = numbers1.map(num => num * 2);
+
+console.log(doubled); 
+
+
+
+const numbers2 = [10, 20, 40, 50, 30];
+
+
+const result = numbers2.filter(num => num > 20);
+
+console.log(result); 
+
+
+
+const numbers = [10, 20, 40, 50, 30];
+
+const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum); 
+ */
+
+
